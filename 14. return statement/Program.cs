e2af8417c5677060440b9a1a2b799d2bbf12556e
nomvula <nzshabangu95@gmail.com>
2023-09-12 13:55:05 +0200

    Add project files.

---
+++ 14. return statement/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.return_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter number ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Cube(x));
+
+            //freeze
+            Console.ReadLine();
+
+
+        }
+
+        static int Cube(int x)
+        {
+            return x*x*x;
+        }
+
+
+    }
+}
